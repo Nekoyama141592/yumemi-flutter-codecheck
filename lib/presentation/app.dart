af2c84216659ea8c91e9a0e494d0bbb77d1cfe10
Nekoyama141592 <mtaky0904@gmail.com>
2025-09-13 02:59:05 +0900

    remove: バナー

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import '../l10n/app_localizations.dart';
@@ -21,22 +20,8 @@ class App extends StatelessWidget {
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: const [Locale('en'), Locale('ja'), Locale('zh')],
-      home: _flavorBanner(child: MyHomePage(), show: kDebugMode),
+      home: MyHomePage(),
     );
   }
 
-  Widget _flavorBanner({required Widget child, bool show = true}) => show
-      ? Banner(
-          location: BannerLocation.topStart,
-          message: F.name,
-          color: Colors.green.withAlpha(150),
-          textStyle: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 12.0,
-            letterSpacing: 1.0,
-          ),
-          textDirection: TextDirection.ltr,
-          child: child,
-        )
-      : Container(child: child);
 }
