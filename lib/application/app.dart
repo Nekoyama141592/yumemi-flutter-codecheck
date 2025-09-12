621e08f095c912cbf5b41e78fb737c0845a6ff05
Nekoyama141592 <mtaky0904@gmail.com>
2025-09-13 03:13:56 +0900

    add: ダークテーマを初期値にする

--- lib/application/app.dart
+++ lib/application/app.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:yumemi_flutter_codecheck/application/theme/dark_theme.dart';
+import 'package:yumemi_flutter_codecheck/application/theme/light_theme.dart';
+import 'package:yumemi_flutter_codecheck/presentation/notifier/theme/theme_notifier.dart';
 import '../l10n/app_localizations.dart';
 
 import 'flavors.dart';
 import '../presentation/my_home_page.dart';
 
-class App extends StatelessWidget {
+class App extends HookConsumerWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeState = ref.watch(themeProvider);
     return MaterialApp(
       title: F.title,
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: themeState.isDarkMode ? darkTheme : lightTheme,
       localizationsDelegates: const [
         AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
